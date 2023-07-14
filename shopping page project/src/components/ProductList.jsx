2c4119a40593e2a28244a7993b1c21f3bfccb54e
dongyunkim96 <dongyunkim96@gmail.com>
2023-07-14 00:00:23 -0400

    productList done

---
+++ shopping page project/src/components/ProductList.jsx
@@ -0,0 +1,50 @@
+import styles from "./ProductList.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function ProductList({ bookmarkState, setBookmarkState }) {
+    const [data, setData] = useState([]);
+
+    const checkIsBookmarked = (item) => {
+        if (bookmarkState) {
+            return bookmarkState.some((e) -> e.id === item.id);
+        }
+        return false;
+    };
+
+    useEffect(() => {
+        axios
+          .get("http://cozshopping.codestates-seb.link/api/v1/products?count=4")
+          .then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                setData(res.data);
+            }
+            else {
+                throw new Error(`API Error: ${res.status} ${res.statusText}`);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }, []);
+
+    return (
+        <div className={styles.mainbox}>
+            <h1 className={styles.title}>Product List</h1>
+            <div className={styles.listWrapper}>
+                {data.map((item) => {
+                    return (
+                        <item
+                          key={item.id}
+                          item={item}
+                          isBookmarked={checkIsBookmarked(item)}
+                          bookmarkState={bookmarkState}
+                          setBookmarkState={setBookmarkState}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+export default ProductList;
