e679e06eb129d735071efe08ac4b22af31895e60
dongyunkim96 <dongyunkim96@gmail.com>
2023-07-13 22:41:25 -0400

    header and footer done

---
+++ shopping page project/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import styles from "./Header.module.css";
+import logo from "../asserts/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+function Header({ showDropdown, setShowDropdown }) {
+    const handleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <header className={styles.header}>
+            {" "}
+            <Link to="/">
+                <div className={styles.wrapper}>
+                    <img className={styles.logo} src={logo} art="logo" />{" "}
+                    <span className={styles.title}>COZ Shopping</span>{" "}
+                </div>{" "}
+            </Link>
+            <span className={styles.button}></span>
+            <FontAwesomeIcon
+              onClick={handleDropdown}
+              className={styles.button}
+              size="2x"
+              icon={faBars}
+            />
+        </header>
+    );
+}
+export default Header;
