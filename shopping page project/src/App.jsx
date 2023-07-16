df2999ebac3348c015ecd60ddbfc83266e78f022
dongyunkim96 <dongyunkim96@gmail.com>
2023-07-16 06:54:16 -0400

    mainpage done

--- shopping page project/src/App.jsx
+++ shopping page project/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
